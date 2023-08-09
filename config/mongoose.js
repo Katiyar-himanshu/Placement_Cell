81a7587547e6d85adc2db98e041810d71f1b7ead
Katiyar-himanshu <88657534+Katiyar-himanshu@users.noreply.github.com>
2023-08-09 23:18:57 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.set('strictQuery',true);
 // const dotenv = require('.env');
 
-const DB = 'mongodb+srv://HimanshuKatiyar:dyKcTjzl0YfdTiE6@cluster0.dv9w3cp.mongodb.net';
+const DB = 'mongodb+srv://HimanshuKatiyar:dyKcTjzl0YfdTiE6@cluster0.dv9w3cp.mongodb.net/Placement_cell';
 
 // dotenv.config({ path: 'config/.env' });
 
