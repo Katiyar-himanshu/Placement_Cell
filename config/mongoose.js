ff7ec47d3263e8a36042024f817bba1c3fb1993c
Katiyar-himanshu <88657534+Katiyar-himanshu@users.noreply.github.com>
2023-08-09 23:39:33 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.set('strictQuery',true);
 // const dotenv = require('.env');
 
-const DB = 'mongodb+srv://HimanshuKatiyar:dyKcTjzl0YfdTiE6@cluster0.dv9w3cp.mongodb.net/test.companies';
+const DB = 'mongodb+srv://HimanshuKatiyar:dyKcTjzl0YfdTiE6@cluster0.dv9w3cp.mongodb.net/?retryWrites=true&w=majority';
 
 // dotenv.config({ path: 'config/.env' });
 
